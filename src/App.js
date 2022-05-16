ec0f54314a172b32c79c6c843934728fc1b563b5
Nathan-freelance-dev <nathanfreelancedev@gmail.com>
2022-05-16 11:14:07 -0700

    Project completed ✅

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -8,9 +8,46 @@ const api = {
 };
 
 function App() {
+	const [ searchValue, setSearchValue ] = useState('');
+	const [ weather, setWeather ] = useState({});
+
+	const search = evt => {
+		if(evt.key === "Enter") {
+			fetch(`${api.base}weather?q=${searchValue}&unit=metric&APPID=${api.key}`)
+			.then(res => res.json())
+			.then(result => {
+				setWeather(result);
+				console.log(result)
+				setSearchValue("")
+			})
+		}
+	}
+
 	return (
 		<>
-			<div className="app_container cold shadow"></div>
+			<div className="app_container sunny shadow">
+				<div className="container_box text-center">
+					<h1 className="text-light mb-3">My weather app</h1>
+
+					<div className="input_feild">
+						<input className="form-control shadow" onChange={e => setSearchValue(e.target.value)} value={searchValue} placeholder="Search anywhere in the world..." onKeyPress={search} />
+					</div>
+
+					{(typeof weather.main !== "undefined") ? (
+						<div className="weather_container mt-5">
+						<div className="forcast_box">
+							<h1 className="text-light">
+								{Math.round(weather.main.humidity)}&ordm;C
+							</h1>
+
+							<div className="mt-3 text-light weather_stats">
+								{weather.weather[0].main}
+							</div>
+						</div>
+					</div>
+					): ("")}
+				</div>
+			</div>
 		</>
 	);
 }
