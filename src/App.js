26e0c2d0a8eabb0c53f8c1e5bc6a28a3a1147335
Nathan-freelance-dev <nathanfreelancedev@gmail.com>
2022-05-16 07:51:23 -0700

    api const created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import './index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const api = {
+    key: "cd69d90bf81a5e0ea340fdf35a4a4ba9",
+    base: "https://api.openweathermap.org/data/2.5/"
+};
+
 function App() {
 	return (
 		<>
