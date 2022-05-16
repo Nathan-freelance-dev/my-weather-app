73e0da90415a48763d314b288a4e2d7db0b6b703
Nathan-freelance-dev <nathanfreelancedev@gmail.com>
2022-05-16 07:50:03 -0700

    Project init -> created a scss file and const with api key and base inside

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import './index.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 	return (
 		<>
-
+			<div className="app_container cold shadow"></div>
 		</>
 	);
 }
